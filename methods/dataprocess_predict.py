3612630f7b19ed57225a2e52fb87475377c8c093
shamsabz <behrouz.shamsaei@uc.edu>
2020-04-21 18:41:06 -0400

    dataprocess_predict changed to loop over proteins once

--- methods/dataprocess_predict.py
+++ methods/dataprocess_predict.py
@@ -10,8 +10,7 @@ import json
 #input must be a .csv file
 #positive_position_file_name is an csv file
 
-
-def getMatrixInputFromJson(inputText, organism, window_size=51, empty_aa = '*'):
+def getMatrixInputFromJsonPrev(inputText, organism, window_size=51, empty_aa = '*'):
     # input format  proteinName, postion, shortsequence,
     prot = []  # list of protein name
     pos = []  # list of position with protein name
@@ -126,6 +125,54 @@ def getMatrixInputFromJson(inputText, organism, window_size=51, empty_aa = '*'):
             short_seqs.append(shortseq)
             # coding = one_hot_concat(shortseq)
             # all_codings.append(coding)
+
+    all_label = [0] *5 + [1]*(len(short_seqs)-5)
+    targetY = kutils.to_categorical(all_label)
+
+    ONE_HOT_SIZE = 21
+    # _aminos = 'ACDEFGHIKLMNPQRSTVWY*'
+    letterDict = {}
+    letterDict["A"] = 0
+    letterDict["C"] = 1
+    letterDict["D"] = 2
+    letterDict["E"] = 3
+    letterDict["F"] = 4
+    letterDict["G"] = 5
+    letterDict["H"] = 6
+    letterDict["I"] = 7
+    letterDict["K"] = 8
+    letterDict["L"] = 9
+    letterDict["M"] = 10
+    letterDict["N"] = 11
+    letterDict["P"] = 12
+    letterDict["Q"] = 13
+    letterDict["R"] = 14
+    letterDict["S"] = 15
+    letterDict["T"] = 16
+    letterDict["V"] = 17
+    letterDict["W"] = 18
+    letterDict["Y"] = 19
+    letterDict["*"] = 20
+
+    # print len(short_seqs)
+    Matr = np.zeros((len(short_seqs), window_size, ONE_HOT_SIZE))
+    samplenumber = 0
+    for seq in short_seqs:
+        AANo = 0
+        for AA in seq:
+            index = letterDict[AA]
+            # print index
+            Matr[samplenumber][AANo][index] = 1
+            # print samplenumber
+            AANo = AANo+1
+        samplenumber = samplenumber + 1
+
+    return Matr, targetY, prot, pos, full_names, names
+
+
+def getMatrixInputFromJson(prot, pos, full_names, names, short_seqs,window_size=51, empty_aa = '*'):
+    # input format  proteinName, postion, shortsequence,
+
 
     all_label = [0] *5 + [1]*(len(short_seqs)-5)
     targetY = kutils.to_categorical(all_label)
--- predict.py
+++ predict.py
@@ -9,6 +9,8 @@ matplotlib.use('Agg')
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import re
+import urllib
 from flask import Flask, make_response, send_from_directory, request, render_template, url_for, redirect
 from sklearn import metrics
 from sklearn import preprocessing
@@ -85,7 +87,7 @@ def predict_for_deepphos(file_name,sites,predictFrame = 'general',
     result.to_csv(outputfile + "prediction_phosphorylation.txt", index=False, header=None, sep='\t',
                   quoting=csv.QUOTE_NONNUMERIC)
 
-def predict_for_deepphos_from_json(input, organism):
+def predict_for_deepphos_from_json_prev(input, organism):
     '''
 
     :param train_file_name: input of your prdict file
@@ -105,14 +107,14 @@ def predict_for_deepphos_from_json(input, organism):
     win3 = 15
 
 
-    from methods.dataprocess_predict import getMatrixInputFromJson
+    from methods.dataprocess_predict import getMatrixInputFromJsonPrev
 
     print('running X_test1 ----------------------------')
-    [X_test1,y_test,ids,position,full_names,names] = getMatrixInputFromJson(input, organism, win1)
+    [X_test1,y_test,ids,position,full_names,names] = getMatrixInputFromJsonPrev(input, organism, win1)
     print('running X_test2 ----------------------------')
-    [X_test2,_,_,_,_,_] = getMatrixInputFromJson(input, organism, win2)
+    [X_test2,_,_,_,_,_] = getMatrixInputFromJsonPrev(input, organism, win2)
     print('running X_test3 ----------------------------')
-    [X_test3,_,_,_,_,_]  = getMatrixInputFromJson(input, organism, win3)
+    [X_test3,_,_,_,_,_]  = getMatrixInputFromJsonPrev(input, organism, win3)
 
     print('----------------------------')
     result_json = {}
@@ -168,6 +170,214 @@ def predict_for_deepphos_from_json(input, organism):
         except:
             print("there was an error")
             result_json[kin] = json.dumps([{'target':"", 'full_name':"", 'position':"", 'score':""}])
+
+    print(result_json)
+    return json.dumps(result_json)
+
+def predict_for_deepphos_from_json(input, organism):
+    '''
+
+    :param train_file_name: input of your prdict file
+                            it must be a .csv file and theinput format  is proteinName, postion,sites, shortseq
+    :param sites: the sites predict: site = 'S','T' OR 'Y'
+    :param predictFrame: 'general' or 'kinase'
+    :param hierarchy: if predictFrame is kinse: you must input the hierarchy:
+            group,family,subfamily,kinase to choose corresponding model
+    :param kinase: kinase name
+    :return:
+     a file with the score
+    '''
+    print("----------------------------")
+    print("in predict_for_deepphos")
+    win1 = 51
+    win2 = 33
+    win3 = 15
+    #window_size = 51
+
+    empty_aa = '*'
+    prot = []  # list of protein name
+    pos = []  # list of position with protein name
+    full_names = []
+    names = []
+    rawseq = []
+    # all_label = []
+    protein_info = {}
+
+    short_seqs = []
+    short_seqs.append([])
+    short_seqs.append([])
+    short_seqs.append([])
+
+    #half_len = int((window_size - 1) / 2)
+
+    # with open(positive_position_file_name, 'r') as rf:
+    #     reader = csv.reader(rf)
+    inputItems = input.split(",")
+    # f.stream.seek(0)
+    # stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+    # reader = csv.reader(stream)
+    print("==========================")
+    for row in inputItems:
+        print(row)
+        modification = re.match(r"[^[]*\[([^]]*)\]", row).groups()[0]
+        # print(re.match(r"[^[]*\[([^]]*)\]", row))
+        print(modification)
+        sseq = ""
+        position = 0
+        center = 0
+        protein = row.split("[")[0]
+        position = int(modification.split("@")[1])
+        input_center = str(modification.split("@")[0])[-1]
+
+        if protein not in protein_info.keys():
+            url2 = "http://eh3.uc.edu/pinet/api/uniprotdb/organism/{}/accession/{}".format(organism, protein)
+            try:
+                with urllib.request.urlopen(url2) as url:
+                    response2 = json.loads(url.read().decode())
+                    print(response2)
+
+                    sseq = response2["sequence"]
+                    protein_info[protein] = response2
+                    center = sseq[position - 1]
+                    gene = response2["primary_gene_name"][0]
+
+
+            except:
+                protein_info[protein]["sequence"] = "*"
+
+                protein_info[protein]["gene"] = "*"
+
+
+        else:
+            sseq = protein_info[protein]["sequence"]
+            gene = protein_info[protein]["primary_gene_name"][0]
+            center = sseq[position - 1]
+
+        # response = urllib.urlopen(url)
+        #
+        # data = json.loads(response.read())
+
+
+
+
+
+
+        # sseq = row[2]
+        # position = int(row[1])
+        # center = sseq[position-1]
+
+        print("------------------------------")
+        print(protein)
+        print(position)
+        print(sseq)
+        print(center)
+        print(input_center)
+
+        sites = 'S', 'T', 'Y'
+        if center in sites:
+            prot.append(protein)
+            pos.append(position)
+            full_name = protein + "[p" + center + "@" + str(position) + "](" + gene + ")"
+            name = protein + "[ph" + center + "@" + str(position) + "]"
+            names.append(name)
+            full_names.append(full_name)
+            rawseq.append(sseq)
+            # print rawseq
+
+            # short seq
+            for window in [0, 1, 2]:
+                if window == 0:
+                    window_size = 51
+                if window == 1:
+                    window_size = 33
+                if window == 2:
+                    window_size = 15
+                half_len = int((window_size - 1) / 2)
+                if position - half_len > 0:
+                    start = position - half_len
+                    left_seq = sseq[start - 1:position - 1]
+                else:
+                    left_seq = sseq[0:position - 1]
+
+                end = len(sseq)
+                if position + half_len < end:
+                    end = position + half_len
+                right_seq = sseq[position:end]
+
+                if len(left_seq) < half_len:
+                    nb_lack = half_len - len(left_seq)
+                    left_seq = ''.join([empty_aa for count in range(nb_lack)]) + left_seq
+
+                if len(right_seq) < half_len:
+                    nb_lack = half_len - len(right_seq)
+                    right_seq = right_seq + ''.join([empty_aa for count in range(nb_lack)])
+                shortseq = left_seq + center + right_seq
+                short_seqs[window].append(shortseq)
+                # coding = one_hot_concat(shortseq)
+                # all_codings.append(coding)
+
+    from methods.dataprocess_predict import getMatrixInputFromJson
+    print('running X_test1 ----------------------------')
+    [X_test1,y_test,ids,position,full_names,names] = getMatrixInputFromJson(prot, pos, full_names, names, short_seqs[0], win1)
+    print('running X_test2 ----------------------------')
+    [X_test2,_,_,_,_,_] = getMatrixInputFromJson(prot, pos, full_names, names, short_seqs[1], win2)
+    print('running X_test3 ----------------------------')
+    [X_test3,_,_,_,_,_]  = getMatrixInputFromJson(prot, pos, full_names, names, short_seqs[2], win3)
+
+    print('----------------------------')
+    result_json = {}
+    #     print X_test1.shape
+#     print len(position)
+
+    from methods.model_n import model_net
+    model = model_net(X_test1, X_test2, X_test3, y_test,nb_epoch = 0)
+
+    #load model weight
+    # if predictFrame == 'general':
+    #
+    #     if site == ('S','T'):
+    #         outputfile = 'general_S_T'
+    #         model_weight = './models/model_general_S,T.h5'
+    #     if site == 'Y':
+    #         outputfile = 'general_Y'
+    #         model_weight = './models/model_general_Y.h5'
+
+    #for kin in ['family_CDK']:
+
+    for kin in ['family_CDK','family_CK2','family_MAPK','family_PKC','family_Src',
+                   'group_AGC','group_Atypical','group_CAMK','group_CMGC','group_TK',
+                   'kinase_CDC2','kinase_CK2a1','kinase_PKACa','kinase_PKCa','kinase_SRC',
+                   'subfamily_CDC2','subfamily_CDK2','subfamily_ERK1','subfamily_PKCa']:
+    # if predictFrame == 'kinase':
+    #     outputfile = 'kinase_{:s}_{:s}'.format(hierarchy, kinase)
+    #     model_weight = './models/model_{:s}_{:s}.h5'.format(hierarchy, kinase)
+        print(kin)
+        outputfile = 'kinase_{:s}'.format(kin)
+
+        model_weight = './models/model_{:s}.h5'.format(kin)
+        print("-----------------------------------------------------")
+        print(model_weight)
+
+        try:
+            model.load_weights(model_weight)
+            predictions_t = model.predict([X_test1, X_test2, X_test3])
+
+            results_ST = np.column_stack((ids, names, full_names ,position,predictions_t[:, 1]))
+
+            result = pd.DataFrame(results_ST, columns= ['target', 'name', 'full_name', 'position', 'score'])
+            print(result.to_json(orient='records'))
+            result_json[kin] = result.to_json(orient='records')
+        #print(result_json)
+
+
+
+
+
+            # result.to_csv(outputfile + "_api_prediction_phosphorylation.txt", index=False, header=None, sep='\t',
+            #               quoting=csv.QUOTE_NONNUMERIC)
+        except:
+            print("there was an error")
+            result_json[kin] = json.dumps([{'target':"", 'name':"", 'full_name':"", 'position':"", 'score':""}])
 
     print(result_json)
     return json.dumps(result_json)
